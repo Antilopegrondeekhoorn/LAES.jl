15d484b0f60bc0a5b2a998e505cfaec5781f8031
Anton <anton.elst@outlook.com>
2022-07-07 15:13:07 +0200

    state struct added

--- src/extra_file.jl
+++ src/extra_file.jl
@@ -1,1 +1,25 @@
-my_f(x,y) = 2x+3y +2
+using CoolProp
+using Unitful: m, J, kPa, Pa, kg, K, W, °C, l, bar, MW, g, uconvert, °, ustrip, s, kW, hr,mol,kJ
+using Plots
+using DataFrames
+using CurveFit
+
+mutable struct State
+    p::Float64
+    T::Float64
+    h::Float64
+    s::Float64
+    mdot::Float64
+    frac_N2::Float64
+    function State(p,T,frac_N2,mdot)
+        s = new()
+        s.p = p
+        s.T = T
+        s.frac_N2 = frac_N2
+        frac_O2 = 1-frac_N2
+        s.h = CoolProp.PropsSI("H","T",T,"P|gas",p,"PR::Nitrogen[$frac_N2]&Oxygen[$frac_O2]")
+        s.s = CoolProp.PropsSI("S","T",T,"P|gas",p,"PR::Nitrogen[$frac_N2]&Oxygen[$frac_O2]")
+        s.mdot = mdot
+        return s
+    end
+end
