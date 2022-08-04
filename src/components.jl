467e2c75fc20c21cc455cfe3902be2032173febd
Anton <anton.elst@outlook.com>
2022-08-05 00:13:54 +0200

    Extra components added

--- src/components.jl
+++ src/components.jl
@@ -1,12 +1,102 @@
 include("helper_functions.jl")
 
-function isentropic_compressor(state_in,p_out,η_c)
+function isentropic_compressor(state_in::AirState,p_out,η_c)
     T_out_is = isentropic_T(state_in,p_out)
     h_out_is = CoolProp.PropsSI("H","T",T_out_is,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.frac_N2)]&Oxygen[$(1-state_in.frac_N2)]")
     h_out_real = (ustrip(h_out_is)-state_in.h)/η_c + state_in.h
     T_out_real = T_real(T_out_is,state_in,p_out,h_out_real)
     
-    state_out = State(ustrip(p_out),ustrip(T_out_real),state_in.frac_N2,state_in.mdot)
+    state_out = State("Air",ustrip(p_out),ustrip(T_out_real),state_in.mdot;frac_N2 = state_in.frac_N2)
     return state_out
 end
 
+function intercooler(state_in_air::AirState,state_in_oil::OilState,pinch_IC,pressureloss_IC)
+    #pinch at the cold end of the intercooler???
+    T_out_air = state_in_oil.T+pinch_IC
+    #pressure loss
+    p_out_air = state_in_air.p - state_in_air.p*pressureloss_IC
+    #output state
+    state_out_air = State("Air",p_out_air,T_out_air,state_in_air.mdot;frac_N2 = state_in_air.frac_N2)
+    
+    #energy balance
+    h_out_oil = (2*(state_in_air.mdot/state_in_oil.mdot)*(state_in_air.h-state_out_air.h)) + state_in_oil.h
+    T_out_oil = T_Essotherm650(h_out_oil)
+    state_out_oil = State("Essotherm650",100000,T_out_oil,state_in_oil.mdot)
+    
+    return state_out_air,state_out_oil
+end
+
+function separator(state_in,yield)
+    Tout, y_N2,x_N2,y_O2,x_O2 = flash_calculation(state_in.p,state_in.frac_N2,yield,"1atm") 
+
+    h_liquid = CoolProp.PropsSI("H","P|liquid",state_in.p,"T",Tout,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+    h_vapor = CoolProp.PropsSI("H","P|gas",state_in.p,"T",Tout,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+    hout = h_liquid*yield + h_vapor*(1-yield)
+    
+    s_liquid = CoolProp.PropsSI("S","P|liquid",state_in.p,"T",Tout,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+    s_vapor = CoolProp.PropsSI("S","P|gas",state_in.p,"T",Tout,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+    sout = s_liquid*yield + s_vapor*(1-yield)
+    
+    liquidstate = State("Air",state_in.p,ustrip(Tout),state2.mdot*yield;frac_N2 = x_N2) #wrong calculation of h and s
+    liquidstate.h = ustrip(h_liquid)
+    liquidstate.s = ustrip(s_liquid)
+
+    vaporstate = State("Air",state_in.p,ustrip(Tout),state2.mdot*(1-yield);frac_N2 = y_N2) #wrong calculation of h and s
+    vaporstate.h = ustrip(h_vapor)
+    vaporstate.s = ustrip(s_vapor)
+    
+    return liquidstate,vaporstate
+end
+
+function isentropic_cryoexpander(state_in::AirState,p_out,η_e)
+    #isentropic calculations
+    yield_guess_is = 0.88
+    while true
+        T_out, y_N2,x_N2,y_O2,x_O2 = flash_calculation(p_out,state_in.frac_N2,yield_guess_is,"1atm") 
+
+        h_liquid = CoolProp.PropsSI("H","P|liquid",ustrip(p_out),"T",T_out,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+        h_vapor = CoolProp.PropsSI("H","P|gas",ustrip(p_out),"T",T_out,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+        global h_out_is = h_liquid*yield_guess_is + h_vapor*(1-yield_guess_is)
+        
+        s_liquid = CoolProp.PropsSI("S","P|liquid",ustrip(p_out),"T",T_out,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+        s_vapor = CoolProp.PropsSI("S","P|gas",ustrip(p_out),"T",T_out,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+        s_out_is = s_liquid*yield_guess_is + s_vapor*(1-yield_guess_is)
+        
+        if abs(state_in.s-ustrip(s_out_is)) <0.2 #arbitrary value
+            global yield_is = yield_guess_is
+            break
+        elseif ustrip(s_out_is) < state_in.s
+            yield_guess_is -=0.00005
+        elseif ustrip(s_out_is) > state_in.s
+            yield_guess_is +=0.00005
+        end
+    end
+    h_out_real = state_in.h- η_e*(state_in.h-ustrip(h_out_is))
+    
+    # Real calculations
+    yield_guess_real = 0.854
+    while true
+        global T_out, y_N2,x_N2,y_O2,x_O2 = flash_calculation(p_out,state_in.frac_N2,yield_guess_real,"1atm") 
+        
+        h_liquid = CoolProp.PropsSI("H","P|liquid",ustrip(p_out),"T",T_out,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+        h_vapor = CoolProp.PropsSI("H","P|gas",ustrip(p_out),"T",T_out,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+        global h_out = h_liquid*yield_guess_real + h_vapor*(1-yield_guess_real)
+        
+        s_liquid = CoolProp.PropsSI("S","P|liquid",ustrip(p_out),"T",T_out,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+        s_vapor = CoolProp.PropsSI("S","P|gas",ustrip(p_out),"T",T_out,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+        global s_out = s_liquid*yield_guess_real + s_vapor*(1-yield_guess_real)
+
+        if abs(h_out_real-ustrip(h_out)) <25
+            global yield = yield_guess_real
+            break
+        elseif ustrip(h_out) < h_out_real
+            yield_guess_real -=0.0001
+        elseif ustrip(h_out) > h_out_real
+            yield_guess_real +=0.0001
+        end
+    end
+    state_out = State("Air",ustrip(p_out),ustrip(T_out),state_in.mdot;frac_N2=y_N2)
+    state_out.h = ustrip(h_out) #State doesn't calculate in 2phase domain
+    state_out.s = ustrip(s_out)
+    return state_out,yield
+end
--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -5,16 +5,21 @@ using Plots
 using DataFrames
 using CurveFit
 
+########################################################
 # State struct
-mutable struct State
+########################################################
+
+mutable struct AirState
+    fluid::String
     p::Float64
     T::Float64
     h::Float64
     s::Float64
     mdot::Float64
     frac_N2::Float64
-    function State(p,T,frac_N2,mdot)
+    function AirState(p,T,mdot,frac_N2)
         s = new()
+        s.fluid = "Air"
         s.p = p
         s.T = T
         s.frac_N2 = frac_N2
@@ -25,6 +30,45 @@ mutable struct State
         return s
     end
 end
+mutable struct OilState
+    fluid::String
+    p::Float64
+    T::Float64
+    h::Float64
+    mdot::Float64
+    function OilState(p,T,mdot)
+        s = new()
+        s.fluid = "Essotherm650"
+        s.p = p
+        s.T = T
+        
+        SpecificHeat = [0.0 1770; 20.0 1850; 40.0 1920; 60.0 1990; 80.0 2060; 100.0 2130; 150.0 2310; 200.0 2490; 250.0 2670; 300.0 2850; 320.0 2920]
+        p = poly_fit(SpecificHeat[:,1] + ones(size(SpecificHeat)[1],1)*273.15,SpecificHeat[:,2], 2)
+        cp(T) = p[1]+p[2]*T+p[3]*T^2
+        h(T) = ((cp(T)+cp(273.15))/2)*(T-273.15)
+        s.h = h(T)
+  
+        s.mdot = mdot
+
+        return s
+    end   
+end
+
+function State(fluid,p,T,mdot;frac_N2=nothing)
+    if fluid == "Air"
+        return AirState(p,T,mdot,frac_N2)
+    elseif fluid == "Essotherm650"
+        return OilState(p,T,mdot)
+    else 
+        @warn("Fluid not known")
+    end
+end
+ 
+########################################################
+# Other helper functions
+########################################################
+
+# Compressors and expanders
 
 function isentropic_T(state_in,p_out)
     # a guess of the isentropic temperature at higher/lower pressure
@@ -121,4 +165,82 @@ function T_real(T_isentropic,state_in,p_out,h_out_real)
         end   
     end
     return T_out_real
+end
+
+# Intercoolers
+
+function T_Essotherm650(h)
+    T = 288:0.001:800
+    hs = []
+    for i = 1:length(T)
+        x = h_Essotherm650(T[i])
+        push!(hs,x)
+    end
+    index = findfirst(x->x>h,hs)
+    return T[index]
+end
+
+# Cryo-expander 
+
+function interpolate_K(T,air_component,pressure_in_words)
+    T = ustrip(T)
+    if pressure_in_words == "1atm"
+        Ks_N2 = [78 0.0798;80 0.3040;82 0.5282;84 0.7582;86 0.9766;88 1.2008;90 1.4249]
+        Ks_O2 = [78 -1.13368;80 -1.1164;82 -0.8959;84 -0.6755;86 -0.4550;88 -0.2346;90 -0.0141]
+    elseif pressure_in_words == "2atm"
+        Ks_N2 = [84 0.7048;86 0.9030;88 1.1012;90 1.2994;92 1.4976;94 1.6958;96 1.8939]
+        Ks_O2 = [84 -0.4573;86 -0.3016;88 -0.1459;90 0.0098;92 0.1655;94 0.3211;96 0.4768]
+    elseif pressure_in_words == "5atm"
+        Ks_N2 = [94 1.5503;96 1.7017;98 1.8531;100 2.0045;102 2.1559;104 2.3073;106 2.4588;108 2.6102]
+        Ks_O2 = [94 0.6605;96 0.7877;98 0.9148;100 1.042;102 1.1692;104 1.2963;106 1.4235;108 1.5506]
+    end
+    
+    if air_component == "Nitrogen"
+        index = findfirst(x-> abs(x-T)<=2,Ks_N2[:,1])
+        T1 = Ks_N2[index,1]
+        T2 = Ks_N2[index+1,1]
+        value1 = Ks_N2[index,2]
+        value2 = Ks_N2[index+1,2]
+    elseif air_component == "Oxygen"
+        index = findfirst(x-> abs(x-T)<=2,Ks_O2[:,1])
+        T1 = Ks_O2[index,1]
+        T2 = Ks_O2[index+1,1]
+        value1 = Ks_O2[index,2]
+        value2 = Ks_O2[index+1,2]
+    else 
+        @warn("Component of the air not known. Air is considered a mixture of Nitrogen and Oxygen")
+    end
+    new_value = value1 + (T-T1)/(T2-T1)*(value2-value1)
+    return new_value
+end
+
+function flash_calculation(p,frac_N2,yield,pressure_in_words) #p given with units
+    Tbubble = CoolProp.PropsSI("T","P",p,"Q",0,"PR::Nitrogen[$(frac_N2)]&Oxygen[$(1-frac_N2)]")
+    Tdew = CoolProp.PropsSI("T","P",p,"Q",1,"PR::Nitrogen[$(frac_N2)]&Oxygen[$(1-frac_N2)]")
+
+    #Assume 80K (between Tbubble and Tdew)
+    Tguess = ((round(ustrip(Tbubble))+round(ustrip(Tdew)))/2)*1.0K
+    p = ustrip(p) #[Pa]
+    while true
+        global L_on_F = yield
+        
+        global K_nitrogen = exp(interpolate_K(Tguess,"Nitrogen",pressure_in_words))/(p/101300)
+        global K_oxygen = exp(interpolate_K(Tguess,"Oxygen",pressure_in_words))/(p/101300)
+
+        global flash = (frac_N2/(1+(L_on_F*((1/K_nitrogen)-1)))) + ((1-frac_N2)/(1+(L_on_F*((1/K_oxygen)-1))))
+        if abs(flash-1) < 0.0005
+            break
+        elseif flash > 1
+            Tguess -= 0.01K
+        elseif flash < 1
+            Tguess += 0.01K
+        end
+    end
+    Tfinal = Tguess
+    x_N2 = (1-K_oxygen)/(K_nitrogen-K_oxygen) #mole fraction of nitrogen in liquid phase
+    y_N2 = K_nitrogen*x_N2 #mole fraction of nitrogen in vapor phase
+
+    x_O2 = 1-x_N2
+    y_O2 = 1-y_N2
+    return Tfinal, y_N2,x_N2,y_O2,x_O2
 end
