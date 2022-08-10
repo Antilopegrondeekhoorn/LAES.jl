fc2363d7253fe35030d49fbb10a56ed41b689d87
Anton <anton.elst@outlook.com>
2022-08-10 18:17:04 +0200

    Components added + charge/discharge cycle

--- src/components.jl
+++ src/components.jl
@@ -1,58 +1,47 @@
 include("helper_functions.jl")
 
-function isentropic_compressor(state_in::AirState,p_out,η_c)
+#########################################################################
+# Charging cycle
+#########################################################################
+
+function isentropic_compressor(state_in,p_out,η_c)
     T_out_is = isentropic_T(state_in,p_out)
-    h_out_is = CoolProp.PropsSI("H","T",T_out_is,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.frac_N2)]&Oxygen[$(1-state_in.frac_N2)]")
+    h_out_is = CoolProp.PropsSI("H","T",T_out_is,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.y_N2)]&Oxygen[$(1-state_in.y_N2)]")
     h_out_real = (ustrip(h_out_is)-state_in.h)/η_c + state_in.h
     T_out_real = T_real(T_out_is,state_in,p_out,h_out_real)
     
-    state_out = State("Air",ustrip(p_out),ustrip(T_out_real),state_in.mdot;frac_N2 = state_in.frac_N2)
+    state_out = State("Air",ustrip(p_out),ustrip(T_out_real),state_in.mdot;phase = state_in.phase,y_N2 = state_in.y_N2,x_N2 = state_in.x_N2,liquid_fraction = state_in.liquid_fraction)
     return state_out
 end
 
-function intercooler(state_in_air::AirState,state_in_oil::OilState,pinch_IC,pressureloss_IC)
+function intercooler(func::String,state_in_air::AirState,state_in_oil::OilState,pinch,pressureloss)
     #pinch at the cold end of the intercooler???
-    T_out_air = state_in_oil.T+pinch_IC
+    if func == "Cool"
+        T_out_air = state_in_oil.T+pinch
+    elseif func == "Heat"
+        T_out_air = state_in_oil.T-pinch
+    else
+        @warn("Function of the intercooler not defined. The intercooler can 'Cool' or 'Heat' the air stream.")
+    end
     #pressure loss
-    p_out_air = state_in_air.p - state_in_air.p*pressureloss_IC
+    p_out_air = state_in_air.p - state_in_air.p*pressureloss
     #output state
-    state_out_air = State("Air",p_out_air,T_out_air,state_in_air.mdot;frac_N2 = state_in_air.frac_N2)
+    state_out_air = State("Air",p_out_air,T_out_air,state_in_air.mdot;phase = state_in_air.phase,y_N2 = state_in_air.y_N2,x_N2 = state_in_air.x_N2,liquid_fraction = state_in_air.liquid_fraction)
     
-    #energy balance
-    h_out_oil = (2*(state_in_air.mdot/state_in_oil.mdot)*(state_in_air.h-state_out_air.h)) + state_in_oil.h
+    #energy balance 
+    h_out_oil = ((state_in_air.mdot/state_in_oil.mdot)*(state_in_air.h-state_out_air.h)) + state_in_oil.h
     T_out_oil = T_Essotherm650(h_out_oil)
     state_out_oil = State("Essotherm650",100000,T_out_oil,state_in_oil.mdot)
     
     return state_out_air,state_out_oil
 end
 
-function separator(state_in,yield)
-    Tout, y_N2,x_N2,y_O2,x_O2 = flash_calculation(state_in.p,state_in.frac_N2,yield,"1atm") 
-
-    h_liquid = CoolProp.PropsSI("H","P|liquid",state_in.p,"T",Tout,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
-    h_vapor = CoolProp.PropsSI("H","P|gas",state_in.p,"T",Tout,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
-    hout = h_liquid*yield + h_vapor*(1-yield)
-    
-    s_liquid = CoolProp.PropsSI("S","P|liquid",state_in.p,"T",Tout,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
-    s_vapor = CoolProp.PropsSI("S","P|gas",state_in.p,"T",Tout,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
-    sout = s_liquid*yield + s_vapor*(1-yield)
-    
-    liquidstate = State("Air",state_in.p,ustrip(Tout),state2.mdot*yield;frac_N2 = x_N2) #wrong calculation of h and s
-    liquidstate.h = ustrip(h_liquid)
-    liquidstate.s = ustrip(s_liquid)
-
-    vaporstate = State("Air",state_in.p,ustrip(Tout),state2.mdot*(1-yield);frac_N2 = y_N2) #wrong calculation of h and s
-    vaporstate.h = ustrip(h_vapor)
-    vaporstate.s = ustrip(s_vapor)
-    
-    return liquidstate,vaporstate
-end
 
 function isentropic_cryoexpander(state_in::AirState,p_out,η_e)
     #isentropic calculations
     yield_guess_is = 0.88
     while true
-        T_out, y_N2,x_N2,y_O2,x_O2 = flash_calculation(p_out,state_in.frac_N2,yield_guess_is,"1atm") 
+        T_out, y_N2,x_N2,y_O2,x_O2 = flash_calculation(p_out,state_in.y_N2,yield_guess_is,"1atm") 
 
         h_liquid = CoolProp.PropsSI("H","P|liquid",ustrip(p_out),"T",T_out,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
         h_vapor = CoolProp.PropsSI("H","P|gas",ustrip(p_out),"T",T_out,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
@@ -76,7 +65,7 @@ function isentropic_cryoexpander(state_in::AirState,p_out,η_e)
     # Real calculations
     yield_guess_real = 0.854
     while true
-        global T_out, y_N2,x_N2,y_O2,x_O2 = flash_calculation(p_out,state_in.frac_N2,yield_guess_real,"1atm") 
+        global T_out, y_N2,x_N2,y_O2,x_O2 = flash_calculation(p_out,state_in.y_N2,yield_guess_real,"1atm") 
         
         h_liquid = CoolProp.PropsSI("H","P|liquid",ustrip(p_out),"T",T_out,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
         h_vapor = CoolProp.PropsSI("H","P|gas",ustrip(p_out),"T",T_out,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
@@ -87,7 +76,7 @@ function isentropic_cryoexpander(state_in::AirState,p_out,η_e)
         global s_out = s_liquid*yield_guess_real + s_vapor*(1-yield_guess_real)
 
         if abs(h_out_real-ustrip(h_out)) <25
-            global yield = yield_guess_real
+            global yield_real = yield_guess_real
             break
         elseif ustrip(h_out) < h_out_real
             yield_guess_real -=0.0001
@@ -95,8 +84,96 @@ function isentropic_cryoexpander(state_in::AirState,p_out,η_e)
             yield_guess_real +=0.0001
         end
     end
-    state_out = State("Air",ustrip(p_out),ustrip(T_out),state_in.mdot;frac_N2=y_N2)
-    state_out.h = ustrip(h_out) #State doesn't calculate in 2phase domain
-    state_out.s = ustrip(s_out)
-    return state_out,yield
+    state_out = State("Air",ustrip(p_out),ustrip(T_out),state_in.mdot;phase = "2phase",y_N2 = y_N2,x_N2 = x_N2,liquid_fraction = yield_real)
+    return state_out
 end
+
+function separator(state_in::AirState)
+    liquidstate = State("Air",state_in.p,state_in.T,state_in.liquid_fraction;phase = "liquid",y_N2 = 0,x_N2 = state_in.x_N2,liquid_fraction = 1) #wrong calculation of h and s
+    vaporstate = State("Air",state_in.p,state_in.T,1-state_in.liquid_fraction;phase = "gas",y_N2 = state_in.y_N2,x_N2 = 0,liquid_fraction = 0) #wrong calculation of h and s
+    return liquidstate,vaporstate
+end
+
+function coldbox(coolant1_min::CoolantState,coolant1_max::CoolantState,coolant2_min::CoolantState,coolant2_max::CoolantState,state_in_air::AirState,state_out_air::AirState,state_in_air_feedback::AirState) #to calculate statecoldout
+    #energy balance
+    h_air_out_feedback = ((state_in_air.mdot*(state_in_air.h-state_out_air.h) - coolant1_min.mdot*(coolant1_max.h-coolant1_min.h) - coolant2_min.mdot*(coolant2_max.h-coolant2_min.h))/state_in_air_feedback.mdot)+state_in_air_feedback.h
+    p_out_intermediate = state_in_air_feedback.p-state_in_air_feedback.p*0.01 #2 times 1% procent, because 2 HEX
+    p_out = p_out_intermediate-p_out_intermediate*0.01
+    
+    T_air_out_feedback_guess = 300.001K #[K]
+    iterations = 0
+    prev_steps = []
+    while true
+        iterations +=1
+        h_guess = CoolProp.PropsSI("H","P|gas",p_out,"T",T_air_out_feedback_guess,"PR::Nitrogen[$(state_in_air_feedback.y_N2)]&Oxygen[$(1-state_in_air_feedback.y_N2)]")
+        
+        #change step when closer to solution
+        diff = abs(h_air_out_feedback-ustrip(h_guess))
+        if diff > 5000
+            step = 2K
+        elseif 1000 < diff < 5000
+            step = 1K
+        elseif 500 < diff < 1000
+            step = 0.5K
+        elseif 200 < diff < 500
+            step = 0.1K
+        elseif 100 < diff < 200
+            step = 0.05K
+        elseif 50 < diff < 100
+            step = 0.01K
+        else
+            step = 0.001K
+        end
+        
+        #change the temperature guess
+        if diff < 1
+            global T_air_out_feedback = T_air_out_feedback_guess
+            break
+        elseif ustrip(h_guess) < h_air_out_feedback
+            T_air_out_feedback_guess += step
+            push!(prev_steps,+1)
+        elseif ustrip(h_guess) > h_air_out_feedback
+            T_air_out_feedback_guess -= step
+            push!(prev_steps,-1)
+        end
+
+        #prevent being in an infinite loop
+        if iterations%4 == 0
+            if sum(prev_steps) ==  0
+                @warn "Infinite loop: adjust the steps"
+                break
+            end
+            prev_steps = []
+        end 
+    end
+    state_out = State("Air",p_out,ustrip(T_air_out_feedback),state_in_air_feedback.mdot;phase = state_in_air_feedback.phase,y_N2 = state_in_air_feedback.y_N2,x_N2 = state_in_air_feedback.x_N2,liquid_fraction = state_in_air_feedback.liquid_fraction)
+    return state_out
+end
+
+#########################################################################
+# Discharge cycle
+#########################################################################
+
+function isentropic_cryopump(state_in,p_out,η_pump)
+    T_out_is = isentropic_T(state_in,p_out)
+    h_out_is = CoolProp.PropsSI("H","T",T_out_is,"P|liquid",ustrip(p_out),"PR::Nitrogen[$(state_in.x_N2)]&Oxygen[$(1-state_in.x_N2)]")
+    h_out_real = (ustrip(h_out_is)-state_in.h)/η_pump + state_in.h
+    T_out_real = T_real(T_out_is,state_in,p_out,h_out_real)
+    
+    state_out = State("Air",ustrip(p_out),ustrip(T_out_real),state_in.mdot;phase = "gas",y_N2 = state_in.x_N2,x_N2 =0.0,liquid_fraction = 0.0)
+    return state_out
+end
+
+function heater_coldstorage(state_in::AirState,coolant_max::CoolantState,pinch,pressure_loss)
+    state_out = State("Air",state_in.p+state_in.p*pressure_loss,coolant_max.T-pinch,state_in.mdot;phase = state_in.phase,y_N2 = state_in.y_N2,x_N2 = state_in.x_N2,liquid_fraction = state_in.liquid_fraction)
+    return state_out
+end
+
+function isentropic_expander(state_in,p_out,η_e)
+    T_out_is = isentropic_T(state_in,p_out)
+    h_out_is = CoolProp.PropsSI("H","T",T_out_is,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.y_N2)]&Oxygen[$(1-state_in.y_N2)]")
+    h_out_real = state_in.h- η_e*(state_in.h-ustrip(h_out_is))
+    T_out_real = T_real(T_out_is,state_in,p_out,h_out_real)
+    state_out = State("Air",ustrip(p_out),ustrip(T_out_real),state_in.mdot;phase = "gas",y_N2 = state_in.y_N2,x_N2 = state_in.x_N2,liquid_fraction = state_in.liquid_fraction)
+    return state_out
+end   
--- src/helper_functions.jl
+++ src/helper_functions.jl
@@ -10,26 +10,58 @@ using CurveFit
 ########################################################
 
 mutable struct AirState
-    fluid::String
+    phase::String #gas,liquid or 2phase
     p::Float64
     T::Float64
     h::Float64
     s::Float64
     mdot::Float64
-    frac_N2::Float64
-    function AirState(p,T,mdot,frac_N2)
-        s = new()
-        s.fluid = "Air"
+    y_N2::Float64 #gas phase N2 fraction
+    x_N2::Float64 #liquid phase N2 fraction
+    liquid_fraction::Float64 
+    function AirState(phase,p,T,mdot,y_N2,x_N2,liquid_fraction)
+        s = new() 
         s.p = p
         s.T = T
-        s.frac_N2 = frac_N2
-        frac_O2 = 1-frac_N2
-        s.h = CoolProp.PropsSI("H","T",T,"P|gas",p,"PR::Nitrogen[$frac_N2]&Oxygen[$frac_O2]")
-        s.s = CoolProp.PropsSI("S","T",T,"P|gas",p,"PR::Nitrogen[$frac_N2]&Oxygen[$frac_O2]")
+        if phase == "gas"
+            s.phase = "gas"
+            s.y_N2 = y_N2
+            y_O2 = 1-y_N2
+            s.h = CoolProp.PropsSI("H","T",T,"P|gas",p,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+            s.s = CoolProp.PropsSI("S","T",T,"P|gas",p,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]") 
+            s.x_N2 = 0
+            s.liquid_fraction = 0
+        elseif phase == "liquid"
+            s.phase = "liquid"
+            s.x_N2 = x_N2
+            x_O2 = 1-x_N2
+            s.h = CoolProp.PropsSI("H","T",T,"P|liquid",p,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+            s.s = CoolProp.PropsSI("S","T",T,"P|liquid",p,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+            s.y_N2 = 0
+            s.liquid_fraction = 1
+        elseif phase == "2phase"
+            s.phase = "2phase"
+            s.y_N2 = y_N2
+            y_O2 = 1-y_N2
+            s.x_N2 = x_N2
+            x_O2 = 1-x_N2
+            
+            h_liquid = CoolProp.PropsSI("H","P|liquid",p,"T",T,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+            h_vapor = CoolProp.PropsSI("H","P|gas",p,"T",T,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+            s.h = h_liquid*liquid_fraction + h_vapor*(1-liquid_fraction)
+    
+            s_liquid = CoolProp.PropsSI("S","P|liquid",p,"T",T,"PR::Nitrogen[$x_N2]&Oxygen[$x_O2]")
+            s_vapor = CoolProp.PropsSI("S","P|gas",p,"T",T,"PR::Nitrogen[$y_N2]&Oxygen[$y_O2]")
+            s.s = s_liquid*liquid_fraction + s_vapor*(1-liquid_fraction)
+            s.liquid_fraction = liquid_fraction
+        else
+            @warn("Phase not implemented.")
+        end
         s.mdot = mdot
         return s
     end
 end
+
 mutable struct OilState
     fluid::String
     p::Float64
@@ -49,20 +81,38 @@ mutable struct OilState
         s.h = h(T)
   
         s.mdot = mdot
-
         return s
     end   
 end
 
-function State(fluid,p,T,mdot;frac_N2=nothing)
+mutable struct CoolantState
+    fluid::String
+    p::Float64
+    T::Float64
+    h::Float64
+    mdot::Float64
+    function CoolantState(fluid,p,T,mdot)
+        s = new()
+        s.fluid = fluid
+        s.p = p
+        s.T = T
+        s.h = CoolProp.PropsSI("H","T",T,"P",p,fluid)
+        s.mdot = mdot
+        return s
+    end   
+end
+function State(fluid,p,T,mdot;phase=nothing,y_N2=nothing,x_N2=nothing,liquid_fraction=nothing)
     if fluid == "Air"
-        return AirState(p,T,mdot,frac_N2)
+        return AirState(phase,p,T,mdot,y_N2,x_N2,liquid_fraction)
     elseif fluid == "Essotherm650"
         return OilState(p,T,mdot)
+    elseif fluid == "Methanol" || fluid == "Propane"
+        return CoolantState(fluid,p,T,mdot)
     else 
         @warn("Fluid not known")
     end
 end
+    
  
 ########################################################
 # Other helper functions
@@ -73,15 +123,21 @@ end
 function isentropic_T(state_in,p_out)
     # a guess of the isentropic temperature at higher/lower pressure
     if ustrip(p_out) > state_in.p
-        T_out_is_guess = (state_in.T+100)K
+        T_out_is_guess = (state_in.T+10)K
     else
-        T_out_is_guess = (state_in.T-100)K
+        T_out_is_guess = (state_in.T-10)K
     end
     iterations = 0
     prev_steps = []
     while true
         iterations +=1
-        s_is_guess = CoolProp.PropsSI("S","T",T_out_is_guess,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.frac_N2)]&Oxygen[$(1-state_in.frac_N2)]")  #composition doesn't change
+        if state_in.phase == "gas"
+            s_is_guess = CoolProp.PropsSI("S","T",T_out_is_guess,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.y_N2)]&Oxygen[$(1-state_in.y_N2)]")  #composition doesn't change
+        elseif state_in.phase == "liquid"
+            s_is_guess = CoolProp.PropsSI("S","T",T_out_is_guess,"P|liquid",ustrip(p_out),"PR::Nitrogen[$(state_in.x_N2)]&Oxygen[$(1-state_in.x_N2)]")  #composition doesn't change
+        else
+            @warn("The calculations of 'isentropic_T' can't be done in the phase of the input state.")
+        end
         #change step when closer to solution
         diff = abs(ustrip(s_is_guess)-state_in.s) 
         if diff > 200
@@ -98,7 +154,7 @@ function isentropic_T(state_in,p_out)
             step = 0.01K
         end
         #change the temperature guess
-        if diff < 0.05
+        if diff < 0.08
             global T_out_is = T_out_is_guess
             break
         elseif ustrip(s_is_guess) < state_in.s
@@ -126,7 +182,13 @@ function T_real(T_isentropic,state_in,p_out,h_out_real)
     prev_steps = []
     while true
         iterations +=1
-        h_out_real_guess = CoolProp.PropsSI("H","T",T_out_real_guess,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.frac_N2)]&Oxygen[$(1-state_in.frac_N2)]")  #composition doesn't change
+        if state_in.phase == "gas"
+            h_out_real_guess = CoolProp.PropsSI("H","T",T_out_real_guess,"P|gas",ustrip(p_out),"PR::Nitrogen[$(state_in.y_N2)]&Oxygen[$(1-state_in.y_N2)]")  #composition doesn't change
+        elseif state_in.phase == "liquid"
+            h_out_real_guess = CoolProp.PropsSI("H","T",T_out_real_guess,"P|liquid",ustrip(p_out),"PR::Nitrogen[$(state_in.x_N2)]&Oxygen[$(1-state_in.x_N2)]")  #composition doesn't change
+        else
+            @warn("The calculations of 'T_real' can't be done in the phase of the input state.")
+        end
         #change step when closer to solution
         diff = abs(ustrip(h_out_real_guess)-h_out_real) 
         if diff > 5000
@@ -169,8 +231,25 @@ end
 
 # Intercoolers
 
+function h_Essotherm650(T)
+    SpecificHeat = [0.0 1770; 20.0 1850; 40.0 1920; 60.0 1990; 80.0 2060; 100.0 2130; 150.0 2310; 200.0 2490; 250.0 2670; 300.0 2850; 320.0 2920]
+    p = poly_fit(SpecificHeat[:,1] + ones(size(SpecificHeat)[1],1)*273.15,SpecificHeat[:,2], 2)
+    cp(T) = p[1]+p[2]*T+p[3]*T^2
+    h(T) = ((cp(T)+cp(273.15))/2)*(T-273.15)
+    return h(T)
+end
+
 function T_Essotherm650(h)
-    T = 288:0.001:800
+    # Rough search
+    T = 200:1:1000
+    hs = []
+    for i = 1:length(T)
+        x = h_Essotherm650(T[i])
+        push!(hs,x)
+    end
+    index = findfirst(x->x>h,hs)
+    # Fine search
+    T = T[index]-1:0.001:T[index]+1
     hs = []
     for i = 1:length(T)
         x = h_Essotherm650(T[i])
@@ -215,32 +294,91 @@ function interpolate_K(T,air_component,pressure_in_words)
 end
 
 function flash_calculation(p,frac_N2,yield,pressure_in_words) #p given with units
-    Tbubble = CoolProp.PropsSI("T","P",p,"Q",0,"PR::Nitrogen[$(frac_N2)]&Oxygen[$(1-frac_N2)]")
-    Tdew = CoolProp.PropsSI("T","P",p,"Q",1,"PR::Nitrogen[$(frac_N2)]&Oxygen[$(1-frac_N2)]")
+    T_bubble = CoolProp.PropsSI("T","P",p,"Q",0,"PR::Nitrogen[$(frac_N2)]&Oxygen[$(1-frac_N2)]")
+    T_dew = CoolProp.PropsSI("T","P",p,"Q",1,"PR::Nitrogen[$(frac_N2)]&Oxygen[$(1-frac_N2)]")
 
     #Assume 80K (between Tbubble and Tdew)
-    Tguess = ((round(ustrip(Tbubble))+round(ustrip(Tdew)))/2)*1.0K
+    T_guess = ((round(ustrip(T_bubble))+round(ustrip(T_dew)))/2)*1.0K
     p = ustrip(p) #[Pa]
     while true
         global L_on_F = yield
-        
-        global K_nitrogen = exp(interpolate_K(Tguess,"Nitrogen",pressure_in_words))/(p/101300)
-        global K_oxygen = exp(interpolate_K(Tguess,"Oxygen",pressure_in_words))/(p/101300)
+
+        global K_nitrogen = exp(interpolate_K(T_guess,"Nitrogen",pressure_in_words))/(p/101300)
+        global K_oxygen = exp(interpolate_K(T_guess,"Oxygen",pressure_in_words))/(p/101300)
 
         global flash = (frac_N2/(1+(L_on_F*((1/K_nitrogen)-1)))) + ((1-frac_N2)/(1+(L_on_F*((1/K_oxygen)-1))))
         if abs(flash-1) < 0.0005
             break
         elseif flash > 1
-            Tguess -= 0.01K
+            T_guess -= 0.001K
         elseif flash < 1
-            Tguess += 0.01K
+            T_guess += 0.001K
         end
     end
-    Tfinal = Tguess
-    x_N2 = (1-K_oxygen)/(K_nitrogen-K_oxygen) #mole fraction of nitrogen in liquid phase
+    T_final = T_guess
+    x_N2 = (1-K_oxygen)/(K_nitrogen-K_oxygen)#mole fraction of nitrogen in liquid phase
     y_N2 = K_nitrogen*x_N2 #mole fraction of nitrogen in vapor phase
 
     x_O2 = 1-x_N2
     y_O2 = 1-y_N2
-    return Tfinal, y_N2,x_N2,y_O2,x_O2
+    return T_final, y_N2,x_N2,y_O2,x_O2
 end
+
+# Extra function for the charging cycle
+function compare(sol1::AirState,sol2::AirState)
+    if sol1.p == sol2.p && sol1.T == sol2.T && sol1.mdot == sol2.mdot && sol1.y_N2 == sol2.y_N2 && sol1.x_N2 == sol2.x_N2 && sol1.phase == sol2.phase
+        return true
+    else
+        return false
+    end   
+end
+
+# Extra function for the discharge cycle
+
+function find_T_5R(h_5R,y_N2)
+    T_5R_guess = 430K  
+    iterations = 0
+    prev_steps = []
+    while true
+        iterations +=1
+        h_5R_guess = CoolProp.PropsSI("H","T",T_5R_guess,"P|gas",p_5R,"PR::Nitrogen[$(y_N2)]&Oxygen[$(1-y_N2)]") 
+        #change step when closer to solution
+        diff = abs(ustrip(h_5R_guess)-h_5R) 
+        if diff > 5000
+            step = 2K
+        elseif 1000 < diff < 5000
+            step = 1K
+        elseif 500 < diff < 1000
+            step = 0.5K
+        elseif 200 < diff < 500
+            step = 0.1K
+        elseif 100 < diff < 200
+            step = 0.05K
+        elseif 50 < diff < 100
+            step = 0.01K
+        else
+            step = 0.001K
+        end
+        #change the temperature guess
+        if diff < 1
+            global T_5R = T_5R_guess
+            break
+        elseif ustrip(h_5R_guess) < h_5R
+            T_5R_guess += step
+            push!(prev_steps,+1)
+        elseif ustrip(h_5R_guess) > h_5R
+            T_5R_guess -= step
+            push!(prev_steps,-1)
+        end
+        #prevent being in an infinite loop
+        if iterations%4 == 0
+            if sum(prev_steps) ==  0
+                @warn "Infinite loop: adjust the steps"
+                break
+            end
+            prev_steps = []
+        end   
+    end 
+    return T_5R
+end
+        
